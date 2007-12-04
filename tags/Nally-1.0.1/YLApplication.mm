8b61063073a30066531f697f456aa18a96d28fbb
yllan <yllan@db928f3c-c32d-11dd-ba5c-d3b7c2b3b534>
2007-12-04 13:47:37 +0000

    add files

--- tags/Nally-1.0.1/YLApplication.mm
+++ tags/Nally-1.0.1/YLApplication.mm
@@ -7,7 +7,7 @@
 //
 
 #import "YLApplication.h"
-
+#import "YLController.h"
 
 @implementation YLApplication
 - (void) sendEvent: (NSEvent *) event {
@@ -44,8 +44,15 @@
                   charactersIgnoringModifiers: leftString 
                                     isARepeat: [event isARepeat] 
                                       keyCode:[event keyCode]];
+        } else if (([event modifierFlags] & NSCommandKeyMask) == NSCommandKeyMask && 
+                   ([event modifierFlags] & NSAlternateKeyMask) == 0 && 
+                   ([event modifierFlags] & NSControlKeyMask) == 0 && 
+                   ([event modifierFlags] & NSShiftKeyMask) == 0 && 
+                   [[event characters] intValue] > 0 && 
+                   [[event characters] intValue] < 10) {
+            [_controller selectTabNumber: [[event characters] intValue]];
+            return;
         }
-        
     }
     
     [super sendEvent:event];
